063372d6da666bac0228e19789991fedf3c820c0
Adam Feldman <feldman238@gmail.com>
2017-09-03 13:39:54 -0400

    Add ability to type location string and convert to coordinate

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
 // global variables
-const getLat = document.getElementsByClassName('lat');
-const getLng = document.getElementsByClassName('lng');
-const upperButtonWrapper = document.getElementsByClassName('upper-button-wrapper')[0];
+// const getGeo = document.getElementsByClassName('geo');
+// const upperButtonWrapper = document.getElementsByClassName('upper-button-wrapper')[0];
 const displayBoundaryButton = document.getElementById('display-boundary');
-const getReminder = document.querySelector('.lower-button-wrapper span');
-let address;
+// const getReminder = document.querySelector('.lower-button-wrapper span');
 let polygonCoords = [ // will hold all coord objects
   // {lat: 43.653254, lng: -79.384132},
   // {lat: 43.660492, lng: -79.404731},
@@ -13,127 +11,158 @@ let polygonCoords = [ // will hold all coord objects
 let map;
 let setPolygon;
 
+
 // builds initial map
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     scrollwheel: false,
-    center: {lat: 43.653254, lng: -79.384132},
+    center: { lat: 43.653254, lng: -79.384132 },
     mapTypeId: 'roadmap',
     mapTypeControl: false,
     zoomControlOptions: {
-        position: google.maps.ControlPosition.LEFT_TOP
+      position: google.maps.ControlPosition.LEFT_TOP
     },
     streetViewControlOptions: {
       position: google.maps.ControlPosition.LEFT_TOP
     },
-    styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"visibility":"on"}]},{"featureType":"administrative","elementType":"labels","stylers":[{"visibility":"on"},{"color":"#716464"},{"weight":"0.01"}]},{"featureType":"administrative.country","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape.natural.landcover","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"geometry.stroke","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"visibility":"simplified"}]},{"featureType":"poi.attraction","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"simplified"},{"color":"#a05519"},{"saturation":"-13"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"simplified"},{"color":"#84afa3"},{"lightness":52}]},{"featureType":"water","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"visibility":"on"}]}]
+    styles: googleMapStylesArray
   });
+
 
   // onClick of #display-boundary
   displayBoundaryButton.addEventListener('click', function() {
+    // Empty polygonCoords array
+    polygonCoords.length = 0;
+    // set display boundary button to .inactive
     this.classList.add('inactive');
+    // remove class of inactive from remove boundary button
     document.getElementById('remove-boundary').classList.remove('inactive');
 
-    getNewCoords() // calls getNewCoords()
+    // function that returns a completed promise  
+    convertGeoCodeToCoordsPromise()
+      //  if successful, run, .then
+      .then(function() {
+        const bounds = new google.maps.LatLngBounds();
+        setPolygon = new google.maps.Polygon({
+          paths: polygonCoords,
+          strokeColor: '#5a8c7d',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#5a8c7d',
+          fillOpacity: 0.35
+        });
+        for (let i = 0; i < polygonCoords.length; i++) {
+          bounds.extend(polygonCoords[i]);
+        }
+        // method that centers the polygon on the map
+        map.fitBounds(bounds);
+        // adds the polygon to the map
+        setPolygon.setMap(map);
+      })
+      //  if promise is unsuccessful, run, .catch
+      .catch(function() {
+        console.log('Uh oh! It looks like there was an error 😱');
+      });
+  });
+}
 
-    setPolygon = new google.maps.Polygon({
-      paths: polygonCoords,
-      strokeColor: '#5a8c7d',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#5a8c7d',
-      fillOpacity: 0.35
+function convertGeoCodeToCoordsPromise() {
+  // create new promise
+  const promise = new Promise(function(resolve, reject) {
+    // create new Geocoder Google Maps object
+    const geocoder = new google.maps.Geocoder();
+    // store all .geo elements in variable
+    const geocodeInput = document.querySelectorAll('.geo');
+
+    geocodeInput.forEach((input) => {
+      // transform input value to a coordinate
+      geocoder.geocode({ 'address': input.value }, function(results, status) {
+
+        if (status == google.maps.GeocoderStatus.OK) {
+          const latitude = results[0].geometry.location.lat();
+          const longitude = results[0].geometry.location.lng();
+          // push the coordinate to the polygonCoords array
+          polygonCoords.push(new google.maps.LatLng(latitude, longitude));
+          
+          // once the loop has completed, call the resolve function of the promise
+          if (polygonCoords.length === geocodeInput.length) {
+            resolve();
+          }
+        // runs if there is an error with the user's input values
+        } else {
+          console.error('There was an issue with the GeocoderStatus.');
+          alert('Invalid input values. Try again.');
+        }
+      });
     });
-    addPolygon(); // calls addPolygon();
   });
 
-  getGeocode();
-
-  function getGeocode() {
-    const geocodeInput = document.querySelector('#geocodeInput');
-    const geocodeSubmit = document.querySelector('#geocodeSubmit');
-    geocodeSubmit.addEventListener('click', function(event) {
-      event.preventDefault();
-      address = geocodeInput.value;
-      thing(address);
-    });
-  }
-
-  function thing(searchedAddress) {
-    const geocoder = new google.maps.Geocoder();
-    const address = searchedAddress;
-
-    geocoder.geocode( { 'address': address}, function(results, status) {
-
-      if (status == google.maps.GeocoderStatus.OK) {
-        var latitude = results[0].geometry.location.lat();
-        var longitude = results[0].geometry.location.lng();
-        console.log(latitude, longitude);
-        console.log(searchedAddress);
-      }
-    });
-  }
+  // return function as a fulfilled or unfulfilled promise
+  return promise;
 }
 
+   
 
-function getNewCoords() {
-  const bounds = new google.maps.LatLngBounds();
-  let missingValue = false;
 
-  for(let i = 0; i < getLat.length; i++) {
-    if(getLat[i].value === '' || getLng[i].value === '') {
-      missingValue = true;
-    }
-  }
 
-  if(missingValue !== true) {
-    polygonCoords.length = 0; // empties array before add new coords
-      // loops through all coordinate-objects and passes each to polygonCoords array
-      for(let i = 0; i < getLat.length; i++) {
-        polygonCoords.push(new google.maps.LatLng(parseFloat(getLat[i].value),parseFloat(getLng[i].value))
-      );
-    }
+// function getNewCoords() {
+//   const bounds = new google.maps.LatLngBounds();
+//   let missingValue = false;
 
-    for (let i = 0; i < polygonCoords.length; i++) {
-      bounds.extend(polygonCoords[i]);
-    }
+//   for (let i = 0; i < getLat.length; i++) {
+//     if (getLat[i].value === '' || getLng[i].value === '') {
+//       missingValue = true;
+//     }
+//   }
 
-    // centers map and fits entire polygon in view
-    map.fitBounds(bounds);
+//   if (!missingValue) {
+//     polygonCoords.length = 0; // empties array before add new coords
+//     // loops through all coordinate-objects and passes each to polygonCoords array
+//     for (let i = 0; i < getLat.length; i++) {
+//       // polygonCoords.push(new google.maps.LatLng(parseFloat(getLat[i].value), parseFloat(getLng[i].value))
+//       polygonCoords.push(new google.maps.LatLng(parseFloat(getLat[i].value), parseFloat(getLng[i].value)));
+//     }
 
-  } else {
-    displayBoundaryButton.classList.add('active');
-    displayBoundaryButton.classList.remove('inactive');
-    document.getElementById('remove-boundary').classList.add('inactive');
-    upperButtonWrapper.classList.add('error');
-    polygonCoords.length = 0;
-  }
-}
+//     for (let i = 0; i < polygonCoords.length; i++) {
+//       bounds.extend(polygonCoords[i]);
+//     }
+
+//     // centers map and fits entire polygon in view
+//     map.fitBounds(bounds);
+
+//   } else {
+//     displayBoundaryButton.classList.add('active');
+//     displayBoundaryButton.classList.remove('inactive');
+//     document.getElementById('remove-boundary').classList.add('inactive');
+//     upperButtonWrapper.classList.add('error');
+//     polygonCoords.length = 0;
+//   }
+// }
 
 // adds polygon to map
-function addPolygon() {
-  let missingValue = false;
+// function addPolygon() {
+//   // let missingValue = false;
 
-  for(let i = 0; i < getLat.length; i++) {
-    if(getLat[i].value === '' || getLng[i].value === '') {
-      missingValue = true;
-    }
-  }
+//   // for (let i = 0; i < getLat.length; i++) {
+//   //   if (getLat[i].value === '' || getLng[i].value === '') {
+//   //     missingValue = true;
+//   //   }
+//   // }
 
-  const getError = upperButtonWrapper;
-  setPolygon.setMap(map);
-  if(!upperButtonWrapper.classList.contains('error')) {
-    getReminder.classList.add('active');
-  }
+//   const getError = upperButtonWrapper;
+//   setPolygon.setMap(map);
+//   if (!upperButtonWrapper.classList.contains('error')) {
+//     getReminder.classList.add('active');
+//   }
 
-  if(getError.classList.contains('error') && missingValue === false) {
-    upperButtonWrapper.classList.remove('error');
-    getReminder.classList.add('active');
-  }
-  // calls removePolygon()
-  removePolygon();
-}
+//   if (getError.classList.contains('error') && missingValue === false) {
+//     upperButtonWrapper.classList.remove('error');
+//     getReminder.classList.add('active');
+//   }
+//   // calls removePolygon()
+//   removePolygon();
+// }
 
 // removes polygon from map
 function removePolygon() {
@@ -153,7 +182,7 @@ function addCoordinate() {
 
   addCoordinateButton.addEventListener('click', function() {
     let numOfCoords = document.getElementsByClassName('coordinate-input-wrapper').length;
-    numOfCoords ++; // increment numOfCoords by 1
+    numOfCoords++; // increment numOfCoords by 1
 
     const newInputs = `
         <span>${numOfCoords}</span>
@@ -164,7 +193,7 @@ function addCoordinate() {
 
     const createDiv = document.createElement('div');
     createDiv.setAttribute('class', 'coordinate-input-wrapper');
-    createDiv.innerHTML = newInputs;  lowerButtonWrapper.insertAdjacentElement('beforebegin', createDiv);
+    createDiv.innerHTML = newInputs; lowerButtonWrapper.insertAdjacentElement('beforebegin', createDiv);
   });
 }
 
@@ -175,7 +204,7 @@ function removeCoordinate() {
 
   removeCoordinateButton.addEventListener('click', function() {
     coordinates[coordinates.length - 1].parentNode.removeChild(coordinates[coordinates.length - 1]);
-    numOfCoords --;
+    numOfCoords--;
   });
 }
 
@@ -186,3 +215,11 @@ function init() {
 }
 
 init(); // call init()
+
+
+
+
+
+
+
+
